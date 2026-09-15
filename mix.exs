05bd0561f43f945a932957bdae737019cf0b2fe0
Gabriel Esquilage <roccaesquil19910@gmail.com>
2026-09-15 14:07:12 -0300

    Add test coverage reporting

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule DatacryptRfb.MixProject do
       version: "0.1.0",
       elixir: "~> 1.20",
       start_permanent: Mix.env() == :prod,
+      test_coverage: [summary: [threshold: 20]],
       deps: deps()
     ]
   end
